b786753e4224435eee56655844bffc6744b690fc
Eric de Ruiter <eric@thisisdevelopment.nl>
2020-10-22 12:05:55 +0200

    fix modelfactory instantiation

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,17 @@
 
 namespace ThisIsDevelopment\LaravelFutureFactories;
 
+use Faker\Generator as FakerGenerator;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(\Illuminate\Database\Eloquent\Factory::class, ModelFactory::class);
+        $this->app->singleton(\Illuminate\Database\Eloquent\Factory::class, function ($app) {
+            return ModelFactory::construct(
+                $app->make(FakerGenerator::class), $this->app->databasePath('factories')
+            );
+        });
     }
 }
