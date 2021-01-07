d59ed1657bc7be0e83a290eba1978acbb3edae9a
Eric de Ruiter <eric@thisisdevelopment.nl>
2021-01-07 16:43:36 +0100

    fix autoloading issues when running under phpunit

--- src/ModelFactory.php
+++ src/ModelFactory.php
@@ -8,15 +8,13 @@ class ModelFactory extends \Illuminate\Database\Eloquent\Factory
 {
     public function load($path)
     {
-        $declared = get_declared_classes();
         if (is_dir($path)) {
             foreach (Finder::create()->files()->name('*.php')->in($path) as $file) {
                 require_once $file->getRealPath();
             }
         }
 
-        $loaded = array_diff(get_declared_classes(), $declared);
-        foreach ($loaded as $cls) {
+        foreach (get_declared_classes() as $cls) {
             if (is_subclass_of($cls, \Illuminate\Database\Eloquent\Factories\Factory::class)) {
                 $cls::initOld($this);
             }
